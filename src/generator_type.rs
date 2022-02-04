740cf12e8e232f56c56d8a67e393f99df6fe931c
ogata-k <ogtkzk712@gmail.com>
2022-02-04 22:03:17 +0900

    clean: GeneratorTypeの分類して整理した

--- src/generator_type.rs
+++ src/generator_type.rs
@@ -3,24 +3,30 @@ use serde::Serialize;
 #[derive(Serialize, Debug, PartialEq, Eq, PartialOrd, Clone, Copy)]
 #[serde(rename_all = "kebab-case")]
 pub enum GeneratorType {
+    // primitive
     Int,
     Real,
     Bool,
     AlwaysNull,
-    UnsignedInt,
-    UnsignedReal,
+    // evaluate
     EvalInt,
     EvalReal,
+    // format
     Format,
+    // random select
     SelectInt,
     SelectReal,
     SelectString,
+    // distribution
     DistIntUniform,
     DistRealUniform,
     DistRealNormal,
+    // case-when condition. default value is null.
     When,
-    IncrementId, // 下限のみサポート
+    // date, time and datetime
     DateTime,
     Date,
     Time,
+    // increment value
+    IncrementId,
 }

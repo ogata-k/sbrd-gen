762dd52fce0c5253a7854f53f351a4007e2c13e1
ogata-k <ogtkzk712@gmail.com>
2022-02-26 10:12:27 +0900

    add: パニック情報を取得しやすいように対応を追加

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod cli;
 
 use crate::cli::SbrdGenApp;
 use clap::Parser;
+use human_panic::setup_panic;
 
 fn main() {
+    setup_panic!();
     let app: SbrdGenApp = SbrdGenApp::parse();
     app.run();
 }

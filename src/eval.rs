31e10d846fcdd677b01cf2607d7e6a5d39b3a735
ogata-k <ogtkzk712@gmail.com>
2022-03-05 10:49:36 +0900

    add: evalモジュールのドキュメントを追加

--- src/eval.rs
+++ src/eval.rs
@@ -3,20 +3,29 @@
 use crate::value::{DataValueMap, SbrdBool, SbrdInt, SbrdReal, SbrdString};
 use evalexpr::{eval_boolean, eval_float, eval_int, EvalexprError};
 
+/// Evaluator for `script` and `format`
+/// Script and format is processed by replacing "{key}" with value based on each entry `(key, value)` of context.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Evaluator<'a> {
     script: &'a str,
     context: &'a DataValueMap<&'a str>,
 }
 
+/// Error while evaluate
 pub type EvalError = EvalexprError;
+/// Alias of [`Result`] for [`Evaluator`]
+///
+/// [`Result`]: https://doc.rust-lang.org/std/result/enum.Result.html
+/// [`Evaluator`]: ./struct.Evaluator.html
 pub type EvalResult<T> = Result<T, EvalError>;
+
 impl<'a> Evaluator<'a> {
-    /// scriptはcontextの各エントリー`(key, value)`をもとに"{key}"を`value`で置き換える形で処理される
+    /// Create from script and context
     pub fn new(script: &'a str, context: &'a DataValueMap<&str>) -> Self {
         Self { script, context }
     }
 
+    /// Apply context to the script
     pub fn format_script(&self) -> EvalResult<SbrdString> {
         // @todo スクリプトのフォーマットの正当性を判定できるようにしたい
         let mut replaced_script = self.script.to_string();
@@ -30,14 +39,23 @@ impl<'a> Evaluator<'a> {
         Ok(replaced_script)
     }
 
+    /// Evaluate the script applied the context, as [`SbrdInt`]
+    ///
+    /// [`SbrdInt`]: ../value/type.SbrdInt.html
     pub fn eval_int(&self) -> EvalResult<SbrdInt> {
         eval_int(&self.format_script()?).map(|v| v as SbrdInt)
     }
 
+    /// Evaluate the script applied the context, as [`SbrdReal`]
+    ///
+    /// [`SbrdReal`]: ../value/type.SbrdReal.html
     pub fn eval_real(&self) -> EvalResult<SbrdReal> {
         eval_float(&self.format_script()?).map(|v| v as SbrdReal)
     }
 
+    /// Evaluate the script applied the context, as [`SbrdBool`]
+    ///
+    /// [`SbrdBool`]: ../value/type.SbrdBool.html
     pub fn eval_bool(&self) -> EvalResult<SbrdBool> {
         eval_boolean(&self.format_script()?).map(|v| v as SbrdBool)
     }

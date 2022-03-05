ede4631ee77b57ed7efdf5de946586dd7e97514f
ogata-k <ogtkzk712@gmail.com>
2022-03-05 22:27:41 +0900

    add: valueモジュールのドキュメントを追加

--- src/value.rs
+++ src/value.rs
@@ -1,4 +1,5 @@
 #![deny(missing_debug_implementations)]
+//! Module for value
 
 use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
 use std::collections::BTreeMap;
@@ -7,26 +8,55 @@ use std::fmt;
 use serde::de::{Error, Unexpected, Visitor};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
+/// KVS for this crate
 pub type ValueMap<K, S> = BTreeMap<K, S>;
+/// KVS for [`DataValue`]
+///
+/// [`DataValue`]: ./enum.DataValue.html
 pub type DataValueMap<K> = ValueMap<K, DataValue>;
+
+/// Integer type for this crate
 pub type SbrdInt = i32;
+/// Real type for this crate
 pub type SbrdReal = f32;
+/// Boolean type for this crate
 pub type SbrdBool = bool;
+/// String type for this crate
 pub type SbrdString = String;
+/// DateTime type for this crate
 pub type SbrdDateTime = NaiveDateTime;
+/// Date type for this crate
 pub type SbrdDate = NaiveDate;
+/// Time type for this crate
 pub type SbrdTime = NaiveTime;
 
+/// Default format string for [`SbrdDateTime`]
+///
+/// [`SbrdDateTime`]: ./type.SbrdDateTime.html
 pub const DATE_TIME_DEFAULT_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+/// Default format string for [`SbrdDate`]
+///
+/// [`SbrdDate`]: ./type.SbrdDate.html
 pub const DATE_DEFAULT_FORMAT: &str = "%Y-%m-%d";
+/// Default format string for [`SbrdTime`]
+///
+/// [`SbrdTime`]: ./type.SbrdTime.html
 pub const TIME_DEFAULT_FORMAT: &str = "%H:%M:%S";
 
+/// Value for [`Schema`]
+///
+/// [`Schema`]: ../schema/struct.Schema.html
 #[derive(Debug, PartialEq, Clone)]
 pub enum DataValue {
+    /// Integer
     Int(SbrdInt),
+    /// Real
     Real(SbrdReal),
+    /// Boolean
     Bool(SbrdBool),
+    /// String
     String(String),
+    /// Null
     Null,
 }
 
@@ -175,6 +205,7 @@ impl std::fmt::Display for DataValue {
 }
 
 impl DataValue {
+    /// Convert to String to use when permute with other Strings
     pub fn to_permutation_string(&self) -> String {
         match self {
             DataValue::Int(v) => v.to_string(),
@@ -185,6 +216,7 @@ impl DataValue {
         }
     }
 
+    /// Convert to String to use parse
     pub fn to_parse_string(&self) -> String {
         match self {
             DataValue::Int(v) => v.to_string(),
@@ -195,6 +227,7 @@ impl DataValue {
         }
     }
 
+    /// Convert to String to use when evaluate script and format
     pub fn to_format_value(&self) -> String {
         match self {
             DataValue::Int(v) => v.to_string(),

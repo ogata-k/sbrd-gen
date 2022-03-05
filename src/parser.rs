59f0f487ab30067fd8dec1392506cedafb2141c5
ogata-k <ogtkzk712@gmail.com>
2022-03-05 21:31:12 +0900

    add: parserモジュールのドキュメントを追加

--- src/parser.rs
+++ src/parser.rs
@@ -1,13 +1,23 @@
 #![deny(missing_debug_implementations)]
+//! Module for parser
 
 use crate::error::{IntoSbrdError, SchemaErrorKind, SchemaResult};
 use crate::SchemaBuilder;
 
+/// Trait of parser for a schema
 pub trait SchemaParser {
+    /// Parse from [`&str`]
+    ///
+    /// [`&str`]: https://doc.rust-lang.org/stable/std/str/
     fn parse_from_str(input: &str) -> SchemaResult<SchemaBuilder>;
+
+    /// Parse from [`Read`]
+    ///
+    /// [`Read`]: https://doc.rust-lang.org/std/io/trait.Read.html
     fn parse_from_reader<R: std::io::Read>(rdr: R) -> SchemaResult<SchemaBuilder>;
 }
 
+/// Parser for a schema written as Yaml
 #[derive(Debug, Default, PartialEq, Eq, Copy, Clone)]
 pub struct YamlParser;
 impl SchemaParser for YamlParser {
@@ -23,6 +33,7 @@ impl SchemaParser for YamlParser {
     }
 }
 
+/// Parser for a schema written as Json
 #[derive(Debug, Default, PartialEq, Eq, Copy, Clone)]
 pub struct JsonParser;
 impl SchemaParser for JsonParser {
